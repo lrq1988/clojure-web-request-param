82234308ebe5f160c55d52f4ab84ed845890fa75
Felix Lee <lrq1988@gmail.com>
2013-09-23 16:43:45 +0800

    Create project.clj
    Modify the dependencies and repositories sections of your Leiningen project.clj file, specifying the version of Clojure that you want.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject req-param "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-jetty-adapter "1.2.0"]]
+   :main req-param.core	 
+)
